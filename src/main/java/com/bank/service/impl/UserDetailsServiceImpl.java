75acb7979fdf50da2ea7f3a8217cb7742fcf3682
Mikail Ozkan <ozkan.mkl@gmail.com>
2021-01-10 18:41:10 +0300

    added new codes into UseRepor class

--- src/main/java/com/bank/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/bank/service/impl/UserDetailsServiceImpl.java
@@ -1,14 +1,18 @@
 package com.bank.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.bank.repository.UserRepo;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 	
-	
+	@Autowired
+	UserRepo userRepo;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
