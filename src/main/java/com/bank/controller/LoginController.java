928982957577b4bcdaf1d3c9a8929e6e368a379b
Mikail Ozkan <ozkan.mkl@gmail.com>
2021-01-10 21:32:35 +0300

    Login controller and some other classes are completed

--- src/main/java/com/bank/controller/LoginController.java
+++ src/main/java/com/bank/controller/LoginController.java
@@ -1,30 +1,98 @@
 package com.bank.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bank.model.Role;
+import com.bank.model.User;
+import com.bank.model.UserRole;
+import com.bank.repository.RoleRepo;
+import com.bank.repository.UserRepo;
+import com.bank.request.LoginForm;
 import com.bank.request.SignUpForm;
+import com.bank.response.JwtResponse;
 import com.bank.response.Response;
+import com.bank.util.JwtUtil;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RestController("/auth")
+@RestController
 @RequestMapping("/auth")
 public class LoginController {
 	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	UserRepo userRepo;
+	
+	@Autowired
+	RoleRepo roleRepo;
+	
+	@Autowired
+	PasswordEncoder encoder;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	
 	@PostMapping("/login")
-	public ResponseEntity<Response> registerUser(@Valid @RequestBody LoginForm loginRequest){
+	public ResponseEntity<?> registerUser(@Valid @RequestBody LoginForm loginRequest){
 		
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		String jwt = jwtUtil.generateToken(authentication);
+		return ResponseEntity.ok(new JwtResponse(jwt));
 		
 	}
 	
 	public ResponseEntity<Response> registerUser(@Valid @RequestBody SignUpForm signUpRequest){
+		Response response = new Response();
+		if(userRepo.existsByUsername(signUpRequest.getUsername())) {
+			response.setMessage("Username is already taken!");
+			response.setSuccess(false);
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		}
+		
+		if(userRepo.existsByEmail(signUpRequest.getEmail())) {
+			response.setMessage("Email is already in use");
+			response.setSuccess(false);
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		}
+		
+		//Creating user's account
+		User user = new User(signUpRequest.getFirstName(), signUpRequest.getLastName(), signUpRequest.getUsername(),
+				signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()));
+		
+		Set<UserRole> userRoles = new HashSet<>();
+		Set<String> strRoles = signUpRequest.getRole();
+		strRoles.forEach(roleName -> {
+			Role role = roleRepo.findByName(roleName).orElseThrow(() -> new RuntimeException("User Role not found."));
+			userRoles.add(new UserRole(user, role));
+		});
+		user.setUserRoles(userRoles);
+		userRepo.save(user);
+		response.setMessage("User Registered Successfully!");
+		response.setSuccess(true);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 		
 	}
 
